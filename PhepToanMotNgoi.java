3089a8b9699bb0a322ebae0730d0901d171c182a
thanh tung <teoduongthanh3@gmail.com>
2022-09-12 21:31:48 +0700

    learning_code_java

---
+++ PhepToanMotNgoi.java
@@ -0,0 +1,19 @@
+
+public class PhepToanMotNgoi {
+	public static void main(String[] args) {
+		int a = 5;
+		boolean b = false;
+		System.out.println("-a giá trị =>" + -a);
+		System.out.println("+a giá trị =>" + +a);
+		System.out.println("!b giá trị =>" + !b);
+		System.out.println("------------");
+		System.out.println("a = " + a);
+		System.out.println("++a giá trị => " + ++a);// cộng trước out sau
+		System.out.println("a++ giá trị => " + a++);// out trước cộng sau
+		System.out.println("a = " + a);
+		System.out.println("--a giá trị => " + --a);// trừ trước out sau
+		System.out.println("a-- giá trị => " + a--);// out trước trừ sau
+		System.out.println("a = " + a);
+
+	}
+}
